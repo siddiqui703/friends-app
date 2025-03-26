62bf704661dc5e7992af6a9eedfa94a32eed2672
siddiqui703 <nawabayansiddiqui12@gmail.com>
2025-03-26 21:41:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+alert("My Nme is Abbas, Welcome to our website enjoy this page :)");
+document.getElementById('box').innerHTML=("My Name is ");
+document.write('Abbas');
+console.log("hey me my name is abbas");
+*/
